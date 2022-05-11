0a24fcb8ca3ce779835bbba9f789e4ffc99e8fa4
Andrew Pham <andrew.pham.789@gmail.com>
2022-05-11 16:10:01 -0500

    MAC - Week1 - Assignment finished

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView } from 'react-native';
+import { Text, ScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
 
 class Contact extends Component {
@@ -11,10 +11,10 @@ class Contact extends Component {
   render() {
     return (
       <ScrollView>
-        <Card
+        {/* <Card
           wrapperStyle={{ margin: 20 }}>
           <Card.Title>Contact Information</Card.Title>
-          {/* <Card.Divider /> */}
+          <Card.Divider />
 
           <Text style={{ marginBottom: 10 }}>
             1 Nucamp Way
@@ -23,7 +23,16 @@ class Contact extends Component {
           </Text>
           <Text>Phone: 1-206-555-1234</Text>
           <Text>Email: campsites@nucamp.co</Text>
-
+        </Card> */}
+        <Card
+          title="Contact Information"
+          wrapperStyle={{ margin: 20 }}
+        >
+          <Text>1 Nucamp Way</Text>
+          <Text>Seattle, WA 98001</Text>
+          <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
+          <Text>Phone: 1-206-555-1234</Text>
+          <Text>Email: campsites@nucamp.co</Text>
         </Card>
       </ScrollView>
     )
